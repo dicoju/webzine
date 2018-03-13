ed964c3952cb8dff82bfcb9cd97dc58c74138ff5
dicoju <julien.culieras@gmail.com>
2018-03-13 11:28:55 +0100

    ajout titres modif image fond + anims pour gérer titres en js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+window.addEventListener("load", ready);
+
+function ready() {
+    $(".show-title-2").hide();
+}
+
 var animationTrigger = $('.show-content');
 
 var triggerLeft = $('.content-icon-bar.left');
@@ -32,16 +38,20 @@ timeline.add("start").to(cover, 1, {
   .to(contentIcon, 0.4, {marginTop: "+=10"}, "start");
 
 animationTrigger.click(function(){
-                if($(this).attr('data-toggle') == 'closed') {
-                    $(this).attr('data-toggle', 'opened');
-                    timeline.play();
-                  timelineClick.pause(0, true);
-                }
-                else if($(this).attr('data-toggle', 'opened')) {
-                    $(this).attr('data-toggle', 'closed');
-                   timeline.reverse();
-                  timelineClick.restart();
-                }
-            });
+    if($(this).attr('data-toggle') === 'closed') {
+        $(this).attr('data-toggle', 'opened');
+        timeline.play();
+        timelineClick.pause(0, true);
+        $(".show-title").hide(1000);
+        $(".show-title-2").show(1000);
+    }
+    else if($(this).attr('data-toggle', 'opened')) {
+        $(this).attr('data-toggle', 'closed');
+       timeline.reverse();
+      timelineClick.restart();
+        $(".show-title").show(1000);
+        $(".show-title-2").hide(1000);
+    }
+});
 
 timelineClick.to(contentIcon, 0.6, {opacity: 0, y: 10}, {opacity: 1, y: 0}).to(contentIcon, 0.6, {opacity: 0, y: 10});
